6536543fbfbe21b365f8958c6d2a1cc8d86d77d9
Slava B <slxvx@MacBook-Pro-Slava.local>
2026-02-04 01:15:03 +0200

    add new heroes, fix xp calculation, new UI

--- utils/constants.js
+++ utils/constants.js
@@ -1,23 +1,26 @@
 export default {
-  BASE_URL_OD: 'https://api.opendota.com/api',
-  BASE_URL_ICONS: 'https://steamcdn-a.akamaihd.net',
+  BASE_URL_OD: "https://api.opendota.com/api",
+  BASE_URL_ICONS: "https://steamcdn-a.akamaihd.net",
 
-  MODE_ALL: 'all',
-  MODE_ALL_NORMAL: 'all-normal',
-  MODE_TURBO: '23',
+  MODE_ALL: "all",
+  MODE_ALL_NORMAL: "all-normal",
+  MODE_TURBO: "23",
 
-  FILENAME_HEROES: 'heroes',
-  FILENAME_LEVELS: 'levels',
+  FILENAME_HEROES: "heroes",
+  FILENAME_LEVELS: "levels",
 
-  NO_FILE: 'ENOENT',
+  NO_FILE: "ENOENT",
 
   EXP_NORMAL: 50,
   EXP_TURBO: 25,
+  CHALLENGE_EXP_NORMAL: 1000,
+  CHALLENGE_EXP_TURBO: 750,
+  WINS_FOR_CHALLENGE: 2.75,
 
-  bronze: 'https://i.imgur.com/qRmaCt2.png',
-  silver: 'https://i.imgur.com/YJUwYqj.png',
-  gold: 'https://i.imgur.com/7BqeJOM.png',
-  platinum: 'https://i.imgur.com/KynMBnB.png',
-  master: 'https://i.imgur.com/Y0gYmaj.png',
-  grandmaster: 'https://i.imgur.com/VDLAFvF.png'
-}
+  bronze: "https://i.imgur.com/qRmaCt2.png",
+  silver: "https://i.imgur.com/YJUwYqj.png",
+  gold: "https://i.imgur.com/7BqeJOM.png",
+  platinum: "https://i.imgur.com/KynMBnB.png",
+  master: "https://i.imgur.com/Y0gYmaj.png",
+  grandmaster: "https://i.imgur.com/VDLAFvF.png",
+};
--- utils/processor.js
+++ utils/processor.js
@@ -1,45 +1,55 @@
-import debugWrapper from 'debugjs-wrapper'
+import debugWrapper from "debugjs-wrapper";
 
-import { fetchPlayerHeroes } from './heroes.js'
-import Constants from './constants.js'
-import { LEVELS } from './levels.js'
-import { HEROES } from './heroes.js'
+import { fetchPlayerHeroes } from "./heroes.js";
+import Constants from "./constants.js";
+import { LEVELS } from "./levels.js";
+import { HEROES } from "./heroes.js";
 
-const { debug } = debugWrapper.all('dota:processor')
+const { debug } = debugWrapper.all("dota:processor");
 
 /**
- * 
+ *
  * @param {Array<{[id]: {string, games: number, wins: number, isTurbo: boolean}}>} data
  * @returns {<Array<{name: string, level: number, img: string, badge: string}>}
  */
-const calculate = data => {
-  const results = []
+const calculate = (data) => {
+  const results = [];
 
   for (const id in HEROES) {
-    const { name, img } = HEROES[id]
+    const { name, img } = HEROES[id];
 
-    let totalExp = 0
+    let totalExp = 0;
 
-    data.forEach(set => {
-      const { games, wins, isTurbo } = set[id]
+    data.forEach((set) => {
+      if (!set[id]) {
+        debug(
+          `Player ${id} does not have hero ${id}, set is empty ${JSON.stringify(set)}`,
+        );
+      }
+      const { games, wins, isTurbo } = set[id];
 
-      totalExp += isTurbo ? (games + wins) * Constants.EXP_TURBO : (games + wins) * Constants.EXP_NORMAL
-      totalExp += isTurbo ? (wins / 4) * (Constants.EXP_TURBO * 10) : (wins / 4) * (Constants.EXP_NORMAL * 10)
-    })
+      totalExp += isTurbo
+        ? (games + wins) * Constants.EXP_TURBO
+        : (games + wins) * Constants.EXP_NORMAL;
+      totalExp += isTurbo
+        ? (wins / Constants.WINS_FOR_CHALLENGE) * Constants.CHALLENGE_EXP_TURBO
+        : (wins / Constants.WINS_FOR_CHALLENGE) *
+          Constants.CHALLENGE_EXP_NORMAL;
+    });
 
-    let level, badge
+    let level, badge;
 
     for (let i = 0; i < LEVELS.length; i++) {
       if (i === 30) {
-        level = i
-        badge = Constants[LEVELS[i].badge]
-        break
+        level = i;
+        badge = Constants[LEVELS[i].badge];
+        break;
       }
 
       if (totalExp >= LEVELS[i].exp && totalExp < LEVELS[i + 1].exp) {
-        level = i
-        badge = Constants[LEVELS[i].badge]
-        break
+        level = i;
+        badge = Constants[LEVELS[i].badge];
+        break;
       }
     }
 
@@ -47,59 +57,59 @@ const calculate = data => {
       name,
       level,
       img: `${Constants.BASE_URL_ICONS}${img}`,
-      badge
-    })
+      badge,
+    });
   }
 
   return results.sort((a, b) => {
     if (a.level > b.level) {
-      return -1
+      return -1;
     }
     if (a.level < b.level) {
-      return 1
+      return 1;
     }
-    return 0
-  })
-}
+    return 0;
+  });
+};
 
 /**
- * 
- * @param {Array<string>} ids 
+ *
+ * @param {Array<string>} ids
  * @param {Array<string>} modes
  * @returns {Promise<Array<{name: string, level: number, img: string, badge: string}>>}
  */
 const processor = async (ids, modes) => {
-  debug(`Procssing players: ${ids} for modes: ${modes}`)
+  debug(`Procssing players: ${ids} for modes: ${modes}`);
 
-  const promises = []
+  const promises = [];
 
-  let action
+  let action;
 
-  if (modes.some(mode => mode === Constants.MODE_ALL_NORMAL)) {
-    action = Constants.MODE_ALL_NORMAL
+  if (modes.some((mode) => mode === Constants.MODE_ALL_NORMAL)) {
+    action = Constants.MODE_ALL_NORMAL;
   }
 
-  if (modes.some(mode => mode === Constants.MODE_ALL)) {
-    action = Constants.MODE_ALL
+  if (modes.some((mode) => mode === Constants.MODE_ALL)) {
+    action = Constants.MODE_ALL;
   }
 
-  ids.forEach(id => {
+  ids.forEach((id) => {
     switch (action) {
       case Constants.MODE_ALL:
-        promises.push(fetchPlayerHeroes(id, null))
-        promises.push(fetchPlayerHeroes(id, Constants.MODE_TURBO))
-        break
+        promises.push(fetchPlayerHeroes(id, null));
+        promises.push(fetchPlayerHeroes(id, Constants.MODE_TURBO));
+        break;
       case Constants.MODE_ALL_NORMAL:
-        promises.push(fetchPlayerHeroes(id, null))
-        break
+        promises.push(fetchPlayerHeroes(id, null));
+        break;
       default:
-        modes.forEach(mode => {
-          promises.push(fetchPlayerHeroes(id, mode))
-        })
-        break
+        modes.forEach((mode) => {
+          promises.push(fetchPlayerHeroes(id, mode));
+        });
+        break;
     }
-  })
-  return calculate(await Promise.all(promises))
-}
+  });
+  return calculate(await Promise.all(promises));
+};
 
-export default processor
+export default processor;
